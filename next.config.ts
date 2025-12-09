aa49742306cffccaa015ca8f80147b023615c4c8
TONYSTARKOP <msonu2018@gmail.com>
2025-12-09 23:20:16 +0530

    Remove basePath for custom domain deployment

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 };
 
 export default nextConfig;
